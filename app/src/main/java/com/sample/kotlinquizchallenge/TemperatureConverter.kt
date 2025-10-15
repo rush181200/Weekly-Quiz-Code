0a82958d914e35ad2a6fce6bb9e14fbe3d975574
Rushabh <rushabhm75@gmail.com>
2025-10-15 15:53:45 -0400

    9th question

--- app/src/main/java/com/sample/kotlinquizchallenge/TemperatureConverter.kt
+++ app/src/main/java/com/sample/kotlinquizchallenge/TemperatureConverter.kt
@@ -12,6 +12,18 @@ package com.sample.kotlinquizchallenge
 * 98.6 → 37.0
 * */
 
-fun main() {
+fun convertTemperature(value: Double, converter: (Double) -> Double): Double {
+    return converter(value)
+}
 
+val celsiusToFahrenheit: (Double) -> Double = { c -> c * 9 / 5 + 32 }
+val fahrenheitToCelsius: (Double) -> Double = { f -> (f - 32) * 5 / 9 }
+
+
+fun main() {
+    val cToF = convertTemperature(25.0, celsiusToFahrenheit)
+    val fToC = convertTemperature(98.6, fahrenheitToCelsius)
+
+    println("25.0 → $cToF") 
+    println("98.6 → $fToC")
 }
