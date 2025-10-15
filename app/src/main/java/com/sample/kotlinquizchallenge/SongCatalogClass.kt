d26cca8b10999b752a65fe3218f005b959ae62e8
Rushabh <rushabhm75@gmail.com>
2025-10-15 15:48:09 -0400

    7th question

--- app/src/main/java/com/sample/kotlinquizchallenge/SongCatalogClass.kt
+++ app/src/main/java/com/sample/kotlinquizchallenge/SongCatalogClass.kt
@@ -13,6 +13,25 @@ package com.sample.kotlinquizchallenge
 *
 * */
 
-fun main() {
+class Song(
+    val title: String,
+    val artist: String,
+    val playCount: Int,
+    val durationSeconds: Int
+) {
+     val formattedDuration: String
+        get() {
+            val minutes = durationSeconds / 60
+            val seconds = durationSeconds % 60
+            return "%02d:%02d".format(minutes, seconds)
+        }
 
+    fun isPopular(): Boolean = playCount >= 1000000
+}
+
+fun main() {
+    val song = Song("Bohemian Rhapsody", "Queen", 2500000, 354)
+
+    println("Duration: ${song.formattedDuration}")
+    println("Popular: ${song.isPopular()}")
 }
