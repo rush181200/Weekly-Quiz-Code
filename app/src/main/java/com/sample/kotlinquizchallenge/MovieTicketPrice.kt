c7d7184e89866d912d96661da2fa89cbf67f79fa
Rushabh <rushabhm75@gmail.com>
2025-10-15 15:16:55 -0400

    4th question

--- app/src/main/java/com/sample/kotlinquizchallenge/MovieTicketPrice.kt
+++ app/src/main/java/com/sample/kotlinquizchallenge/MovieTicketPrice.kt
@@ -16,6 +16,18 @@ package com.sample.kotlinquizchallenge
 *
 * */
 
-fun main() {
 
+fun main() {
+    println("25, false → ${calculateTicketPrice(25, isMatinee = false)}")
+    println("70, true → ${calculateTicketPrice(70, isMatinee = true)}")
+    println("8, false → ${calculateTicketPrice(8,  isMatinee = false)}")
+}
+
+fun calculateTicketPrice(age: Int, isMatinee: Boolean): Int {
+    require(age >= 0) { "Age cannot be negative" }
+    return when{
+        age<13 -> 8
+        age in 13..64 -> if(isMatinee) 10 else 15
+        else-> if(isMatinee) 8 else 10
+    }
 }
